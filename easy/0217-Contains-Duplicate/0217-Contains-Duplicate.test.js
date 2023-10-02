7b5243031f88303ab567924d38a2bf70e6b6566c
Jazmin Matos <jmatos1993@gmail.com>
2023-10-02 16:56:00 -0400

    clarified test descriptions

--- easy/0217-Contains-Duplicate/0217-Contains-Duplicate.test.js
+++ easy/0217-Contains-Duplicate/0217-Contains-Duplicate.test.js
@@ -5,7 +5,7 @@ describe('tests for Set Solution', () => {
         {nums: [1,2,3,1], output: true},
         {nums: [1,2,3,4], output: false},
         {nums: [1,1,1,3,3,4,3,2,4,2], output: true},
-    ])('detects duplicate integer', ({nums, output}) => {
+    ])('detects if there is a duplicate integer', ({nums, output}) => {
         expect(solutions.containsDuplicateSetSolution(nums)).toBe(output)
     })
 })
@@ -15,7 +15,7 @@ describe('tests for Loop Solution', () => {
         {nums: [1,2,3,1], output: true},
         {nums: [1,2,3,4], output: false},
         {nums: [1,1,1,3,3,4,3,2,4,2], output: true},
-    ])('detects duplicate integer', ({nums, output}) => {
+    ])('detects if there is a duplicate integer', ({nums, output}) => {
         expect(solutions.containsDuplicateLoopSolution(nums)).toBe(output)
     })
 })
