654018b374ca16086c7b068b4a2be156a4305b5d
Jazmin Matos <jmatos1993@gmail.com>
2023-10-05 20:57:05 -0400

    added valid sudoku solution and tests

--- medium/0036-valid-sudoku/0036-valid-sudoku.js
+++ medium/0036-valid-sudoku/0036-valid-sudoku.js
@@ -12,3 +12,28 @@ Note: A Sudoku board (partially filled) could be valid but is not necessarily so
 Only the filled cells need to be validated according to the mentioned rules.
 */
 
+function isValidSudoku(board) {
+    let seen = new Set();
+    
+    for (let row = 0; row < 9; row++) {
+        for (let column = 0; column < 9; column++) {
+            let num = board[row][column]
+
+            if (num === ".") continue
+
+            if (seen.has(`${num} in row ${row}`) || 
+                seen.has(`${num} in column ${column}`) ||
+                seen.has(`${num} in subBox (${Math.floor(row/3)}/${Math.floor(column/3)})`)) {
+
+                return false
+            } else {
+                seen.add(`${num} in row ${row}`)
+                seen.add(`${num} in column ${column}`)
+                seen.add(`${num} in subBox (${Math.floor(row/3)}/${Math.floor(column/3)})`)
+            }
+        }
+    }
+    return true;
+};
+
+module.exports = isValidSudoku
