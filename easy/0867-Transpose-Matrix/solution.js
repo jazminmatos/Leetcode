4b5485ddb5dfd69388e64dcffd82b72030fd2436
Jazmin Matos <jmatos1993@gmail.com>
2023-10-11 22:10:36 -0400

    added solution for transpose matrix

---
+++ easy/0867-Transpose-Matrix/solution.js
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/transpose-matrix/
+
+/* 
+Given a 2D integer array matrix, return the transpose of matrix.
+
+The transpose of a matrix is the matrix flipped over its main diagonal, switching the matrix's row and column indices.
+*/
+
+var transpose = function (matrix) {
+    const newMatrix = []
+
+    for (let i = 0; i < matrix[0].length; i++) {
+        let currArr = []
+
+        for (let j = 0; j < matrix.length; j++) {
+            currArr.push(matrix[j][i])
+        }
+
+        newMatrix.push(currArr)
+    }
+    return newMatrix
+};
