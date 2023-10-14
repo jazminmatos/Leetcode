10ec125eff989e8d97390872868fa47289fbed3b
Jazmin Matos <jmatos1993@gmail.com>
2023-10-14 18:43:59 -0400

    added different solution with explanation

--- easy/0867-Transpose-Matrix/solution.js
+++ easy/0867-Transpose-Matrix/solution.js
@@ -21,4 +21,24 @@ var transpose = function (matrix) {
     return newMatrix
 };
 
+
+// Solution #2: Creates a fixed nested array
+// Time Complexity: O(n * m)
+// When you create a dynamic array - let a = [] - and add an item to the array, under the hood, it will: 
+// 1. loop over current items in the array, 
+// 2. copy them, 
+// 3. move it to a different location in memory that has more space, so that it can include the newly added item
+// When dynamic arrays need to be moved to expand its size, Array.prototype.push(n) becomes O(n) instead of O(1).
+var transposeWithFixedArray = function (matrix) {
+    let output = new Array(matrix[0].length).fill(null).map(item => new Array(matrix.length).fill(null));
+
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[0].length; j++) {
+            output[j][i] = matrix[i][j];
+        }
+    }
+
+    return output;
+}
+
 module.exports = transpose
