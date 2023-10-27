102fdab6419d3b0c99f00d6ab0553ae91af23ed8
Jazmin Matos <jmatos1993@gmail.com>
2023-10-27 14:38:46 -0400

    optimized solution

--- 1-easy/0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.js
+++ 1-easy/0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.js
@@ -2,19 +2,19 @@
  * @param {string} s
  * @return {string}
  */
-var reverseVowels = function(s) {
+var reverseVowels = function (s) {
     // edge cases:
-        // lowercase & uppercase
-        // if s === null
-        // if s === ""
-        // if s === non-alphanumerical characters like ';/,-='
+    // lowercase & uppercase
+    // if s === null
+    // if s === ""
+    // if s === non-alphanumerical characters like ';/,-='
 
     // create a set w/ vowels
     // Two pointer method: (start, end) iterate through string
-        // if start is NOT a vowel increment start until it is a vowel
-        // if end is NOT a vowel decrement end until it is a vowel
-        // once start and end are BOTH vowels, swap them
-    
+    // if start is NOT a vowel increment start until it is a vowel
+    // if end is NOT a vowel decrement end until it is a vowel
+    // once start and end are BOTH vowels, swap them
+
     let vowels = new Set(['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'])
 
     let start = 0
@@ -22,15 +22,18 @@ var reverseVowels = function(s) {
     let wordArray = s.split("")
 
     while (start < end) {
-        if (vowels.has(wordArray[start]) && vowels.has(wordArray[end])) {
-            let temp = wordArray[start]
-            wordArray[start++] = wordArray[end]
-            wordArray[end--] = temp
+        if (!vowels.has(wordArray[start])) {
+            start++
+            continue
+        }
+        if (!vowels.has(wordArray[end])) {
+            end--
+            continue
         }
 
-        if (!vowels.has(wordArray[start])) start++
-
-        if (!vowels.has(wordArray[end])) end--
+        let temp = wordArray[start]
+        wordArray[start++] = wordArray[end]
+        wordArray[end--] = temp
     }
     return wordArray.join("")
 };
