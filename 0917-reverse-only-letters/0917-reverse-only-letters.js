d737d0c29e48403510b515c18df4b7893ff63033
jazminmatos <65051399+jazminmatos@users.noreply.github.com>
2023-10-27 10:58:55 -0400

    Time: 57 ms (25.77%), Space: 41.7 MB (87.76%) - LeetHub

---
+++ 0917-reverse-only-letters/0917-reverse-only-letters.js
@@ -0,0 +1,35 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseOnlyLetters = function(s) {
+    let start = 0
+    let end = s.length - 1
+    let temp
+
+    let stringArray = s.split("")
+
+    while (start < end) {
+        if (isLetter(start, stringArray[start]) && isLetter(end, stringArray[end])) {
+            temp = stringArray[start]
+            stringArray[start++] = stringArray[end]
+            stringArray[end--] = temp
+        }
+
+        if (!isLetter(start, stringArray[start])) start++
+        if (!isLetter(end, stringArray[end])) end--
+    }
+    return stringArray.join("")
+};
+
+function isLetter(index, s) {
+    // a-z, Ascii Values: 97-122
+    if (s.charCodeAt(0) > 96 && s.charCodeAt(0) < 123) {
+        return true
+    }
+    // A-Z, Ascii Values: 65-90
+    if (s.charCodeAt(0) > 64 && s.charCodeAt(0) < 91) {
+        return true
+    }
+    return false
+}
