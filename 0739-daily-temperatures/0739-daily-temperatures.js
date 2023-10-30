0d5f7bbd9d054a8199fa2e817d2ce5e07e602976
jazminmatos <65051399+jazminmatos@users.noreply.github.com>
2023-10-30 17:08:37 -0400

    Time: 2833 ms (24.19%), Space: 103.1 MB (5.12%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.js
+++ 0739-daily-temperatures/0739-daily-temperatures.js
@@ -4,17 +4,35 @@
  */
 
 // brute force solution
-var dailyTemperatures = function(temperatures) {
-    let result = Array(temperatures.length).fill(0)
+// var dailyTemperatures = function(temperatures) {
+//     let result = Array(temperatures.length).fill(0)
    
-    for (let i = 0; i < temperatures.length; i++) {
-        for (let j = i + 1; j < temperatures.length; j++) {
-            if (temperatures[j] > temperatures[i]) {
-                result[i] = j - i
-                break
-            }
+//     for (let i = 0; i < temperatures.length; i++) {
+//         for (let j = i + 1; j < temperatures.length; j++) {
+//             if (temperatures[j] > temperatures[i]) {
+//                 result[i] = j - i
+//                 break
+//             }
+//         }
+//     }
+
+//     return result
+// };
+
+// Monotonic Stack Solution
+// Keep track of indices w/ stack. Once we encounter temperature which is greater than what is at the top of the stack, we pop out the top of the stack and store the result as 'ans[index] = i - index'.
+var dailyTemperatures = function(temperatures) {
+    let stack = [], ans = Array(temperatures.length).fill(0);
+
+    for(let i = 0; i < temperatures.length; i++) {
+        while(stack.length && temperatures[stack[stack.length - 1]] < temperatures[i]) {
+            console.log(stack[stack.length - 1])
+            console.log(temperatures[stack[stack.length - 1]])
+            let index = stack.pop();
+            ans[index] = i - index;
         }
+        stack.push(i);
     }
 
-    return result
+    return ans;
 };
