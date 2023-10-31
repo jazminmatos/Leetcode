76fb86476360b1d41add3580014f4a2b72afca08
jazminmatos <65051399+jazminmatos@users.noreply.github.com>
2023-10-31 12:42:09 -0400

    Time: 58 ms (40.78%), Space: 42 MB (88.61%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.js
+++ 0682-baseball-game/0682-baseball-game.js
@@ -8,18 +8,18 @@ var calPoints = function(operations) {
 
     for (let i = 0; i < operations.length; i++) {
         if (operations[i] === '+') {
-            stack.push(parseInt(stack[stack.length - 1]) + parseInt(stack[stack.length - 2]))
+            stack.push(stack[stack.length - 1] + stack[stack.length - 2])
         } else if (operations[i] === 'D') {
             stack.push(stack[stack.length - 1] * 2)
         } else if (operations[i] === 'C') {
             stack.pop()
         } else {
-            stack.push(operations[i])
+            stack.push(parseInt(operations[i]))
         }
     }
 
     for (let num of stack) {
-        result += parseInt(num)
+        result += num
     }
     
     return result
